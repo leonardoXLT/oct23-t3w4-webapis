e104a85500d00606a254529522625e9d259ed5a2
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-05-09 19:22:12 +1000

    up remain

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # oct23-t3w4-webapis
+
+<!-- What is this project -->
+
+
+<!-- What does this project use -->
+
+
+<!-- Screenshots and/or deployment URL -->
+
+
+
+## To-Do 
+
+- [x] Cries
+- [x] Shiny chance
+	- [ ] Shiny sound effect
+- [ ] Team generator
+	- [ ] select number of Pokemon to generate in the team 
+
+- [ ] Fetch with headers 
+
+
--- src/pokemonApi.js
+++ src/pokemonApi.js
@@ -37,11 +37,41 @@ async function putDataOnPage(dataToDisplay){
 	
 
 	// Wishlist: add random chance to select front_shiny instead of front_default
+
+	// Real odds are 1 in 8192 
+	// Testing/development odds are 1 in 4
+	// Generate random number between 1 and [odds upper limt]
+	// If number is 1, show shiny
+	// Else, show default
+
 	let imageContainer = document.getElementsByClassName("pokemonImage")[0];
 	let imageElement = imageContainer.getElementsByTagName("IMG")[0];
-	imageElement.src = dataToDisplay.sprites.front_default;
+
+	let oddsUpperLimit = 4;
+	let shinyResult = Math.floor(Math.random() * oddsUpperLimit) + 1;
+
+	if (shinyResult == 1 ) {
+		imageElement.src = dataToDisplay.sprites.front_shiny;
+		console.log("Shiny Pokemon found!");
+	} else {
+		imageElement.src = dataToDisplay.sprites.front_default;
+	}
+
+	
 
 	// document.querySelector(".pokemonImage img").src = dataToDisplay.sprites.front_default;
+
+
+
+	let cryURL = dataToDisplay.cries.latest;
+    let pokemonAudioElement = document.querySelector(".pokemonCry audio")
+	pokemonAudioElement.src = cryURL;
+
+	let pokemonAudioPlayButton = document.querySelector(".pokemonCry");
+	pokemonAudioPlayButton.addEventListener("click", () => {
+		pokemonAudioElement.volume = 0.1;
+		pokemonAudioElement.play();
+	});
 
 
 
